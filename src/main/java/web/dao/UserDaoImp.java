c22c95cab5dbca8da8fb7e3b27a8326c9e7f9b16
DmitriySi <demonishishe@gmail.com>
2022-06-09 13:45:42 +0300

    done

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -14,29 +14,28 @@ import java.util.List;
 @Transactional
 public class UserDaoImp implements UserDao {
 
-    @Qualifier
     @PersistenceContext
     EntityManager entityManager;
 
     public void add(User user) {
         entityManager.persist(user);
     }
-
+    @Transactional
     public void delete(long id) {
         User user = showUser(id);
         entityManager.remove(user);
     }
-
+    @Transactional
     public List<User> listUsers() {
 
         TypedQuery<User> query = entityManager.createQuery("SELECT user FROM User user", User.class);
         return query.getResultList();
     }
-
+    @Transactional
     public User showUser(long id) {
         return entityManager.find(User.class, id);
     }
-
+    @Transactional
     public void update(User user, long id) {
         entityManager.merge(user);
     }
